1b26641af31d8545192a51b0a9994a98058086fc
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-23 15:25:43 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Section 5: Day of the week Challenge+");
+
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
+
+    }
+
+    private static void printDayOfTheWeek(int day){
+        switch(day){
+            case 0:
+                System.out.println("Day of the week is Sunday");
+                break;
+            case 1:
+                System.out.println("Day of the week is Monday");
+                break;
+            case 2:
+                System.out.println("Day of the week is Tuesday");
+                break;
+            case 3:
+                System.out.println("Day of the week is Wednesday");
+                break;
+            case 4:
+                System.out.println("Day of the week is Thursday");
+                break;
+            case 5:
+                System.out.println("Day of the week is Friday");
+                break;
+            case 6:
+                System.out.println("Day of the week is Saturday");
+                break;
+            default:
+                System.out.println("Invalid day!");
+                break;
+        }
     }
 }
